b0fd528b35e54f459562329940dd2a8496c6fd47
weekdawn <571426779@qq.com>
2017-04-07 00:03:46 +0800

    增加图灵机器人API，并且更新为自动回复所有信息

--- qq_robot.py
+++ qq_robot.py
@@ -1,16 +1,40 @@
 #coding=utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import requests
 from qqbot import QQBotSlot as qqbotslot, RunBot
 
 
-i = '你好，我是QQ机器人'
-j = 'QQ机器人已关闭'
+KEY = "f978f03fb6cc4707b9579c68a953060a"
+user_id = "mingo"
+#获取图灵机器人自动回复信息
+def get_response(msg):
+	#信息传送网址
+	api_url = "http://www.tuling123.com/openapi/api"
+
+	data = {
+			'key' : KEY,
+			'info' : msg,
+			'userif' : user_id
+			}
+	try:
+		#向图灵机器人发送请求
+		r = requests.post(api_url,data=data).json()
+		print "requests successfully"
+		#返回自动回复信息
+		return r.get('text')
+	except:
+		return "error in http requests"
+
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    if content == 'hello':
-        bot.SendTo(contact, i.decode('gbk'))
-    elif content == 'stop':
-        bot.SendTo(contact, j.decode('gbk'))
-        bot.Stop()
+	if content == "-stop":
+		bot.SendTo(contact, "qq_robot has stoped")
+		bot.Stop()
+	else:
+		bot.SendTo(contact, get_response(content))
 
 if __name__ == '__main__':
-    RunBot()
+	RunBot()
